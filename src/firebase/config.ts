6fd3aaaeaacf836ccaf4dd6e3776b0b5d0f49753
sambhav jain <vstrader418@gmail.com>
2025-12-22 08:04:05 +0000

    add this code firebase console and do not change anthing from website //

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyB9QBha5EK5_gd7v93MPzPHrHi9AN23TtI",
   authDomain: "vstraders-9be75.firebaseapp.com",
   projectId: "vstraders-9be75",
-  storageBucket: "vstraders-9be75.appspot.com",
+  storageBucket: "vstraders-9be75.firebasestorage.app",
   messagingSenderId: "784126613398",
   appId: "1:784126613398:web:ec9ebdad0d046ee9d02fb7",
   measurementId: "G-XKQD13YY7M"
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,23 +2,33 @@
 import { getApp, getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getAnalytics, type Analytics } from "firebase/analytics";
 import { firebaseConfig } from './config';
 
 let firebaseApp: FirebaseApp;
 let auth: Auth;
 let firestore: Firestore;
+let analytics: Analytics | undefined;
 
 function initializeFirebase() {
   if (getApps().length === 0) {
     firebaseApp = initializeApp(firebaseConfig);
     auth = getAuth(firebaseApp);
     firestore = getFirestore(firebaseApp);
+    if (typeof window !== 'undefined') {
+        analytics = getAnalytics(firebaseApp);
+    }
   } else {
     firebaseApp = getApp();
     auth = getAuth(firebaseApp);
     firestore = getFirestore(firebaseApp);
+    if (typeof window !== 'undefined') {
+        if (!analytics) {
+            analytics = getAnalytics(firebaseApp);
+        }
+    }
   }
-  return { firebaseApp, auth, firestore };
+  return { firebaseApp, auth, firestore, analytics };
 }
 
 export { initializeFirebase };
