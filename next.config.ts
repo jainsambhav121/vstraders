53bf0378fdcb87b7cdfb9c743056dafe133332bc
sambhav jain <vstrader418@gmail.com>
2025-12-24 07:34:17 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://drive.g

--- next.config.ts
+++ next.config.ts
@@ -54,6 +54,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https' as const,
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
