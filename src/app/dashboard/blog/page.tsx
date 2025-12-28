d1fd677c59197477b95f0d0750b54507387106cb
sambhav jain <vstrader418@gmail.com>
2025-12-28 13:42:17 +0000

    fix the working functionality (_for element <LinkComponent>_)

--- src/app/dashboard/blog/page.tsx
+++ src/app/dashboard/blog/page.tsx
@@ -164,8 +164,8 @@ export default function BlogPostsPage() {
                           <DropdownMenuItem asChild>
                             <Link href={`/dashboard/blog/edit/${post.id}`}>Edit</Link>
                           </DropdownMenuItem>
-                          <DropdownMenuItem asChild>
-                             <Link href={`/blog/${post.id}`}>View</Link>
+                          <DropdownMenuItem>
+                             <Link href={`/blog/${post.id}`} className="w-full h-full">View</Link>
                           </DropdownMenuItem>
                           <DropdownMenuSeparator />
                            <AlertDialogTrigger asChild>
